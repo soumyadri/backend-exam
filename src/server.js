c4dacef6e059bb52cdb96d4ddb4ef11feb3e8246
Soumyadri Das <dadsoumyadri@gmail.com>
2022-11-26 23:29:38 +0530

    Backend API create for exam question

--- src/server.js
+++ src/server.js
@@ -7,9 +7,11 @@ app.use(cors());
 const connect = require('./config/db');
 
 const userController = require('./controller/users.controller');
+const questionController = require('./controller/questions.controller');
 
 app.use(express.json());
 app.use('/user', userController);
+app.use('/question', questionController);
 
 const start = async () => {
     await connect();
